80f27809f11dee034a58dbd324e282ee1071aa03
reetesh <reetesh@clayfin.com>
2025-04-02 15:25:25 +0530

    first commit

---
+++ DSA_CPP/BASICS/Index.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using  namespace std;
+
+int main(){
+    cout<<"FASTER"<<endl;
+    int count = 10;
+    for(int i=0;i<count;i++){
+        for(int j=0;j<count;j++){
+            // this is commitemsnt now thos 
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    
+}
